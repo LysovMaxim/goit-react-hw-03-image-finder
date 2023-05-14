bf80afd515da7dc8991192abeb66b25dcfd14d96
LysovMaxim <max_maran@icloud.com>
2023-05-14 17:38:15 +0300

    Замінив назву папки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { Searchbar } from './SearchBar';
+import { Searchbar } from './Searchbar';
 import { ImageGallery } from './ImageGallery';
 import { Button } from './Button';
 import { Loader } from './Loader';
